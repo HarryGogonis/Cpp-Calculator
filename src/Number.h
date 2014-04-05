e821d43213b786e8cd981855f92222c59e05e02d
Haralambos Gogonis <hgogonis@gmail.com>
2014-04-04 20:55:16 -0400

    Moved the number subless header files into Number.h to help work with the polymorphism

--- src/Number.h
+++ src/Number.h
@@ -3,12 +3,76 @@
 
 using namespace std;
 
+#define FRACTION_TYPE "Fraction"
+#define INTEGER_TYPE "Integer"
+#define IRRATIONAL_TYPE "Irrational"
+#define LOG_TYPE "Log"
+
+
+// Base class
 class Number 
 {
 	public:
 		virtual string toString() = 0;	
+		virtual string getType() = 0;
+
 		friend ostream& operator<<(ostream& out, Number* num) 
 		{
 			out << num->toString() << endl;
 		}
 };
+
+class Integer:public Number
+{
+	public:
+		int value;
+
+		Integer(int);
+	
+		string toString();
+		string getType() { return INTEGER_TYPE; };
+};
+
+
+class Fraction:public Number
+{
+	public:
+		Number* num;
+		Number* den;
+
+		Fraction(Number*, Number*);
+		Fraction(int, int);
+		Fraction(string,string);
+		~Fraction();
+
+		string toString();
+		string getType() { return FRACTION_TYPE; };
+};
+
+class Log: public Number{
+	public:
+		Number* base;
+		Number* power;
+
+		Log(Number*, Number*);
+		Log(string, string);
+		Log(int, int);
+		~Log();
+		
+		string toString();
+		string getType() {return LOG_TYPE; };
+};
+
+
+
+class Irrational:public Number
+{
+	public:
+		string name;
+		Irrational(string s):name(s){};
+  		
+  		string toString();
+  		string getType() { return IRRATIONAL_TYPE; };
+
+};
+
