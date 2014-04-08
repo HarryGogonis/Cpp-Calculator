fa17201f137c5056e2a0e21ecc079f6e0ba0869c
Haralambos Gogonis <hgogonis@gmail.com>
2014-04-08 19:51:17 -0400

    added operations files

---
+++ src/Operations.h
@@ -0,0 +1,10 @@
+#include "Number.h"
+
+class Operations {
+	public:
+		static Number* add(Number*, Number*);
+		static Number* subtract(Number*, Number*);
+		static Number* multiply(Number*, Number*);
+		static Number* divide(Number*, Number*);
+
+};
